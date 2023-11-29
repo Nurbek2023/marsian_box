28acb402448d194eab64b049565f77191858819c
nurbek.mamasaliev2023@gmail.com <nurbek.mamasaliev2023@gmail.com>
2023-11-30 00:46:10 +0600

    First comment

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Main
 {
+// Nurbek apandi
 	public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    Random random = new Random();
